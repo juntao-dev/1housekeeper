614dd77a126e8a973e270d58f150a4fc30ab9030
juntao-dev <juntao.dev@gmail.com>
2019-08-24 15:24:38 +1000

    feature/3 add cover video background

--- 1housekeeper-front/src/Components/LandingPage/LandingPage.js
+++ 1housekeeper-front/src/Components/LandingPage/LandingPage.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import CardDecks from './CardDecks';
+import Jumbotron from "react-bootstrap/Jumbotron";
+import CardDecks from "./CardDecks";
+import video from "./video.mp4";
+
+import "./LandingPage.css";
 
 class LandingPage extends React.Component {
-    render() {
-        return (
-            <Jumbotron>
-                <CardDecks />
-            </Jumbotron>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Jumbotron className="videojumbotron" style={{ padding: 0 }}>
+          <video autoplay="autoplay" loop="loop" muted="muted">
+            <source src={video} type="video/mp4" />
+          </video>
+          <Jumbotron style={{ zIndex: 100 }}>
+            <CardDecks />
+          </Jumbotron>
+        </Jumbotron>
+      </div>
+    );
+  }
 }
 
 export default LandingPage;
